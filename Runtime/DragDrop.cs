98ace2d205b1b7b38c9fb33f3a9c6e777ad9092b
vagechirkov <vagechirkov@gmail.com>
2022-02-04 21:33:06 +0100

    fix on scale slider detection

--- Runtime/DragDrop.cs
+++ Runtime/DragDrop.cs
@@ -1,6 +1,7 @@
 using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 namespace TimeEvaluationUI.Runtime
 {
@@ -10,36 +11,58 @@ namespace TimeEvaluationUI.Runtime
         
         RectTransform _rectTransform;
         CanvasGroup _canvasGroup;
+        
+        // Radius of the protractor
+        const float ScaleRadius = 228f;
+        
+        Vector2 StartPosition { get; set; }
+        bool IsOnScale { get; set; }
 
         void Awake()
         {
             _rectTransform = GetComponent<RectTransform>();
             _canvasGroup = GetComponent<CanvasGroup>();
+            StartPosition = transform.localPosition;
         }
 
         public void OnPointerDown(PointerEventData eventData)
         {
-            Debug.Log("OnPointerDown");
+            
         }
 
         public void OnBeginDrag(PointerEventData eventData)
         {
-            Debug.Log("OnBeginDrag");
             _canvasGroup.alpha = .6f;
             _canvasGroup.blocksRaycasts = false;
         }
 
-        public void OnEndDrag(PointerEventData eventData)
-        {
-            Debug.Log("OnEndDrag");
-            _canvasGroup.alpha = 1f;
-            // _canvasGroup.blocksRaycasts = true;
-        }
-
         public void OnDrag(PointerEventData eventData)
         {
-            Debug.Log("OnDrag");
             _rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
         }
+        
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            _canvasGroup.alpha = 1f;
+            
+            var circlePosition = (Vector2) transform.localPosition;
+            var diff = circlePosition - StartPosition;
+            var angle = Vector2.Angle(diff.normalized, Vector2.down);
+            
+            if (Mathf.Abs(diff.magnitude - ScaleRadius) < 10f && angle < 90f)
+            {
+                var circleImage = gameObject.GetComponent<Image>();
+                circleImage.color = Color.green;
+            }
+            else
+            {
+                eventData.pointerDrag.GetComponent<CanvasGroup>().blocksRaycasts = true;
+            }
+                
+            Debug.Log("Angle: " + angle);
+            Debug.Log("Dist: " + diff.magnitude);
+            eventData.pointerDrag.GetComponent<CanvasGroup>().blocksRaycasts = true;
+        }
+
     }
 }
